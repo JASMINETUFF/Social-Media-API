716d262081b9e18588ae65eb5d7895da99d60d3f
Jasmine Santa Maria <89921853+JASMINETUFF@users.noreply.github.com>
2022-02-05 11:13:58 -0800

    Updates to model
    Updated the thoughts.js & reaction js

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,10 +1,12 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
 
 // Schema to create thoughts model
 const thoughtsSchema = new Schema({
 thoughtsText: {
       type: String,
-      required: true,
+      required: 'Sorry! Cant leave this empty. Give us your thoughts!',
       minlength: 1,
       maxlength: 280
     },
@@ -13,15 +15,25 @@ thoughtsText: {
       // Returns the numeric value corresponding to the current time
       default: Date.now,
       // Plugged in Moments JS
-      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')]
+      get: timestamp => dateFormat(timestamp)
     },
-    thoughts: {
-      
+    username: {
+      type: String,
+      required: true      
     },
-    friends: {
+    reactions: [reactionSchema]
   },
-}
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
 );
+
+thoughtsSchema.virtual('reactionCount').get(function(){
+  return this.reactions.length;
+});
 
 const thoughts = model('thoughts', thoughtsSchema);
 
